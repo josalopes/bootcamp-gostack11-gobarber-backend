92fc770eec9fab13269a60496fb9545b394c1736
josafa <josafa.lopes@gmail.com>
2020-05-11 14:25:23 -0300

    Implementado o cadastro de usuários com Model e relacionamentos e utilizando criptografia de senha.

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,36 @@
+import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+
+import User from '../models/users';
+
+interface Request {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({ name, email, password }: Request): Promise<User> {
+    const usersRepository = getRepository(User);
+
+    const checkUserExists = await usersRepository.findOne({
+      where: { email },
+    });
+
+    if (checkUserExists) {
+      throw new Error('E-mail address already in use');
+    }
+
+    const hashedPassword = await hash(password, 8);
+    const user = usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await usersRepository.save(user);
+    return user;
+  }
+}
+
+export default CreateUserService;
