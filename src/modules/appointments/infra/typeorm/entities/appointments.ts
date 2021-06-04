7f2b8356f1166a774d3056990d62df77b3b3b7a1
Francisco Josafá <josafa.lopes@gmail.com>
2021-06-04 10:33:07 -0300

    Refatoranda a implementaçao do build

--- src/modules/appointments/infra/typeorm/entities/appointments.ts
+++ src/modules/appointments/infra/typeorm/entities/appointments.ts
@@ -1,4 +1,3 @@
-import User from '@modules/users/infra/typeorm/entities/User';
 import {
   Entity,
   Column,
@@ -8,6 +7,8 @@ import {
   ManyToOne,
   JoinColumn,
 } from 'typeorm';
+
+import User from '@modules/users/infra/typeorm/entities/User';
 
 @Entity('appointments')
 class Appointment {
