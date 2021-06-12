d1135ebe9f7089604ef77d693c95c1f75695c762
Francisco Josafá <josafa.lopes@gmail.com>
2021-06-12 20:02:58 -0300

    Atualizados o arquivo ormconfig.json

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,13 +3,13 @@ import './MailTemplateProvider';
 import './MailProvider';
 import './CacheProvider';
 
-import { container } from 'tsyringe';
+// import { container } from 'tsyringe';
 
-import ICacheProvider from './CacheProvider/models/ICacheProvider';
-import RedisCacheProvider from './CacheProvider/implementations/RedisCacheProvider';
+// import ICacheProvider from './CacheProvider/models/ICacheProvider';
+// import RedisCacheProvider from './CacheProvider/implementations/RedisCacheProvider';
 
-const providers = {
-  redis: RedisCacheProvider,
-};
+// const providers = {
+//   redis: RedisCacheProvider,
+// };
 
-container.registerSingleton<ICacheProvider>('CacheProvider', providers.redis);
+// container.registerSingleton<ICacheProvider>('CacheProvider', providers.redis);
